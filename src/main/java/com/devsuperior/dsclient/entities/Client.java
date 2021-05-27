7a0c01568fac6c291d06621f05a7407370e1336c
Tiago Gomes <tiagogomes187@gmail.com>
2021-05-26 22:32:21 -0300

    Adding an annotation in the variable 'birthDate'.

--- src/main/java/com/devsuperior/dsclient/entities/Client.java
+++ src/main/java/com/devsuperior/dsclient/entities/Client.java
@@ -23,6 +23,7 @@ public class Client implements Serializable {
 	private String name;
 	private String cpf;
 	private Double income;
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant birthDate;
 	private Integer children;
 	
